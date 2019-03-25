ffdef4b9b51721d233daf60420eca61ba6687d05
jefferson <jeff.otoni@gmail.com>
2019-03-25 14:10:15 -0300

    connect rabbit, pass

--- pkg/rabbitqm/rabbitqm.go
+++ pkg/rabbitqm/rabbitqm.go
@@ -24,7 +24,7 @@ func failOnError(err error, msg string) {
 }
 
 func connect() {
-	conn, err = amqp.Dial("amqp://admin:1234#@localhost:5672/")
+	conn, err = amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 }
